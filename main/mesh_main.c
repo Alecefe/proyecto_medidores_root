6ae2c4eef8fb17f2620204930ef3c5a0baf5aeb3
Alecefe <43854092+Alecefe@users.noreply.github.com>
2019-11-12 13:53:32 -0400

    Update mesh_main.c

--- main/mesh_main.c
+++ main/mesh_main.c
@@ -5,6 +5,9 @@
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
+
+Marco se ha unido al grupo
+
 */
 
 //Esto es una prueba 
